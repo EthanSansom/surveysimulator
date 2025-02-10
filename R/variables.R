5113db123ca90e3a2d73215466aab93d5ba7f7dc
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2025-02-10 18:51:09 -0500

    Set `missing_perc = NA` by default, meaning we don't check missingness at all in validators

--- R/variables.R
+++ R/variables.R
@@ -1,4 +1,4 @@
-# Constructors -----------------------------------------------------------------
+# constructors -----------------------------------------------------------------
 
 new_variable_promise <- function(
     name,
@@ -18,26 +18,28 @@ new_variable_promise <- function(
 }
 
 #' @export
-count <- function(min, max, missing_perc = 0, name = "x") {
+count <- function(min, max, missing_perc = NA, name = "x") {
   force(min)
   force(max)
   force(missing_perc)
   force(name)
 
   x_name <- paste0("`", name, "`") # Make `chk::chk_*()` add ticks to names
+  no_missing <- isTRUE(missing_perc == 0)
+
   validator <- function(x) {
     chk::chk_whole_numeric(x, x_name = x_name)
     chk::chk_range(x, range = c(min, max), x_name = x_name)
     # TODO: Eventually we'll need more robust missing % detection to related
     # missingness between variables (i.e. via survey skip logic)
-    if (missing_perc == 0) chk::chk_not_any_na(x, x_name = x_name)
+    if (no_missing) chk::chk_not_any_na(x, x_name = x_name)
   }
   simulator <- function(size, seed) {
     set.seed(seed)
     out <- sample(seq(min, max), size = size, replace = TRUE)
     simulate_missing_perc(out, missing_perc = missing_perc, seed = seed)
   }
-  example <- commas(c(seq(min, max), if (missing_perc > 0) NA))
+  example <- commas(c(seq(min, max), if (!no_missing) NA))
 
   new_variable_promise(
     name = name,
@@ -49,17 +51,19 @@ count <- function(min, max, missing_perc = 0, name = "x") {
 }
 
 #' @export
-range <- function(min, max, missing_perc = 0, name = "x") {
+range <- function(min, max, missing_perc = NA, name = "x") {
   force(min)
   force(max)
   force(missing_perc)
   force(name)
 
   x_name <- paste0("`", name, "`") # Make `chk::chk_*()` add ticks to names
+  no_missing <- isTRUE(missing_perc == 0)
+
   validator <- function(x) {
     chk::chk_numeric(x, x_name = x_name)
     chk::chk_range(x, range = c(min, max), x_name = x_name)
-    if (missing_perc == 0) chk::chk_not_any_na(x, x_name = x_name)
+    if (no_missing) chk::chk_not_any_na(x, x_name = x_name)
   }
   simulator <- function(size, seed) {
     set.seed(seed)
@@ -70,7 +74,7 @@ range <- function(min, max, missing_perc = 0, name = "x") {
   }
   example <- commas(c(
     round(runif(4, min = min, max = max), 2),
-    if (missing_perc > 0) NA
+    if (!no_missing) NA
   ))
 
   new_variable_promise(
@@ -83,21 +87,22 @@ range <- function(min, max, missing_perc = 0, name = "x") {
 }
 
 #' @export
-binary <- function(missing_perc = 0, name = "x") {
+binary <- function(missing_perc = NA, name = "x") {
   force(missing_perc)
   force(name)
 
   x_name <- paste0("`", name, "`") # Make `chk::chk_*()` add ticks to names
+  no_missing <- isTRUE(missing_perc == 0)
+
   validator <- function(x) {
-    chk::chk_subset(x, values = c(0, 1, if (missing_perc > 0) NA), x_name = x_name)
-    # if (missing_perc == 0) chk::chk_not_any_na(x, x_name = x_name)
+    chk::chk_subset(x, values = c(0, 1, if (!no_missing) NA), x_name = x_name)
   }
   simulator <- function(size, seed) {
     set.seed(seed)
     out <- sample(c(0L, 1L), size = size, replace = TRUE)
     simulate_missing_perc(out, missing_perc = missing_perc, seed = seed)
   }
-  example <- commas(c(0L, 1L, if (missing_perc > 0) NA))
+  example <- commas(c(0L, 1L, if (no_missing) NA))
 
   new_variable_promise(
     name = name,
@@ -110,22 +115,24 @@ binary <- function(missing_perc = 0, name = "x") {
 
 # TODO: Eventually this would be a factor with correct levels or a character
 #' @export
-categorical <- function(values, missing_perc = 0, name = "x") {
+categorical <- function(values, missing_perc = NA, name = "x") {
   force(values)
   force(missing_perc)
   force(name)
 
   x_name <- paste0("`", name, "`") # Make `chk::chk_*()` add ticks to names
+  no_missing <- isTRUE(missing_perc == 0)
+
   validator <- function(x) {
     chk::chk_character(x, x_name = x_name)
-    chk::chk_subset(x, values = c(values, if (missing_perc > 0) NA), x_name = x_name)
+    chk::chk_subset(x, values = c(values, if (!no_missing) NA), x_name = x_name)
   }
   simulator <- function(size, seed) {
     set.seed(seed)
     out <- sample(values, size = size, replace = TRUE)
     simulate_missing_perc(out, missing_perc = missing_perc, seed = seed)
   }
-  example <- commas(c(values, if (missing_perc > 0) NA))
+  example <- commas(c(encodeString(values, quote = '"'), if (!no_missing) NA))
 
   new_variable_promise(
     name = name,
@@ -143,26 +150,43 @@ is_variable_promise <- function(x) {
   inherits(x, cls_nm("variable_promise"))
 }
 
+# declare ----------------------------------------------------------------------
+
+#' @export
+declare_raw_variables <- function(.data, ...) {
+
+  dots <- rlang::enexprs(...)
+  dot_names <- names(dots)
+  stopifnot(is_dataset_promise(.data))
+  caller_env <- rlang::caller_env()
+
+  # Generate the <variable_promise> objects
+  variable_promises <- vector("list", length(dots))
+  for (i in seq_along(dots)) {
+    # The name of the `..i` argument is the name of our variable
+    name <- dot_names[[i]]
+
+    # The value of `..i` is an un-evaluated variable promise generator. We
+    # feed the RHS call the correct name and evaluate
+    generator <- rlang::call_modify(dots[[i]], name = name)
+
+    variable_promises[[i]] <- eval(generator, envir = caller_env)
+  }
+
+  # TODO: Support the over-writing of old variables, currently we'll reach an
+  #       error if you try to do so.
+  attr(.data, "variables") <- append(variable_promises, attr(.data, "variables"))
+  .data
+}
+
 # helpers ----------------------------------------------------------------------
 
+# TODO: Eventually we'll want to model not-at-random missingness
 simulate_missing_perc <- function(x, missing_perc = 0, seed = 1) {
-  if (missing_perc <= 0) {
+  if (is.na(missing_perc) || missing_perc <= 0) {
     return(x)
   }
   set.seed(1)
   x[runif(n = length(x)) <= missing_perc] <- NA
   x
-}
-
-commas <- function(x, n = 5) {
-  x_len <- length(x)
-  if (x_len <= n) {
-    return(paste(x, collapse = ", "))
-  }
-  x <- as.character(x)
-  paste(c(x[1:(n - 2)], "...", x[c(x_len - 1, x_len)]), collapse = ", ")
-}
-
-cls_nm <- function(x) {
-  paste0("surveysimulator_", x)
 }
